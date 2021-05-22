ddef277e16fabea43aef87c961db096ee48164cc
Deepak Goswami <deepakgoswami@quantumphinance.com>
2021-05-22 11:13:01 +0530

    navbar_setup

---
+++ myportfolio/src/components/navBar/navBar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import logo from '../../assest/image/logo.png';
+const navBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/navbar"><img className="logo" src={logo}></img></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/navbar">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/navbar">Experience</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/navbar">Work</a>
+                        </li> 
+                        <li className="nav-item">
+                            <a className="nav-link" href="/navbar">Contact</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/navbar">Resume</a>
+                        </li> 
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default navBar;
