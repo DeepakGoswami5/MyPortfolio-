50974102a88988a05ac1e049cf93b41a8cf63413
Deepak Goswami <deepakgoswami@quantumphinance.com>
2021-05-22 14:02:33 +0530

    added_homepage_styled_homepage

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,12 +1,17 @@
 import './App.scss';
 import NavBar from './components/NavBar/NavBar';
+import HomePage from './components/HomePage/HomePage'
 function App() {
   return (
     <div className="App">
       <div className="sidebar">
         <NavBar/>
       </div> 
-      <div className="main-content"></div>
+      <div className="main-content">
+        <div className="content">
+          <HomePage/>
+        </div>
+      </div>
     </div>
   );
 }
--- myportfolio/src/components/HomePage/HomePage.js
+++ myportfolio/src/components/HomePage/HomePage.js
@@ -1,9 +1,29 @@
 import React from 'react'
-
+import "./HomePage.scss"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faGithubAlt} from '@fortawesome/free-brands-svg-icons'
+import {faLinkedinIn} from '@fortawesome/free-brands-svg-icons'
+import {Link} from 'react-router-dom';
 const HomePage = () => {
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div className="HomePage">
+            <div className="main-header">
+                <h1 className="hero-text"> Hi, I'am 
+                    <span> Deepak Goswami</span>
+                </h1>
+                <p className="a-subtext"> 
+                    LoreFugiat voluptate aliqua mollit adipisicing ullamco eu 
+                    fugiat ea id eu laboris enim qui.
+                </p>
+                <div className="icons">
+                    <Link className="icon-holder">
+                        <FontAwesomeIcon className="icon gt" icon={faGithubAlt} />
+                    </Link>
+                    <Link className="icon-holder">
+                        <FontAwesomeIcon className="icon lin" icon={faLinkedinIn} />
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
