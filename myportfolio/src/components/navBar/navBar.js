acb7f7e75393a7e4a43f893c09177350df4e968a
Deepak Goswami <deepakgoswami@quantumphinance.com>
2021-05-22 11:35:00 +0530

    added_header_added_font_awesome

--- myportfolio/src/components/navBar/navBar.js
+++ myportfolio/src/components/navBar/navBar.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import logo from '../../assest/image/logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
 const navBar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light">
             <div className="container-fluid">
                 <a className="navbar-brand" href="/navbar"><img className="logo" src={logo}></img></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
