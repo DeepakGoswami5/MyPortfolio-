8d47781a01687cae66aa58183f6d35a7ee01cb07
Deepak Goswami <deepakgoswami@quantumphinance.com>
2021-05-23 09:35:20 +0530

    styled_imagesection_added_skillsection

--- myportfolio/src/components/ImageSection/ImageSection.js
+++ myportfolio/src/components/ImageSection/ImageSection.js
@@ -8,26 +8,25 @@ const ImageSection = () => {
                 <img src={about}></img>
             </div>
             <div className="about-info">
-                <h4>I am <span>Lorem</span></h4>
-                <p>Sint labore aute do cupidatat.</p>
-            </div>
-            <div className="about-details"> 
-                <div className="name-details">
-                    <p>Full name    :</p>
-                    <p>Lorem Ipsum</p>
+                <h4>I am <span>Deepak Goswami</span></h4>
+                <p className="about-text">Sint labore aute do cupidatat Sint labore aute do cupidatat Sint labore aute do cupidatat Sint labore aute do cupidatat.</p>
+                <div className="about-details"> 
+                    <div className="left-section">
+                        <p>Name</p>
+                        <p>Age</p>
+                        <p>Address</p>
+                        <p>Nationality</p>
+                        <p>Launguage</p>
+                    </div>
+                    <div className="right-section">
+                        <p>: Deepak goswami</p>
+                        <p>: 25</p>
+                        <p>: 65 Shree Ram Nagar</p>
+                        <p>: Indian</p>
+                        <p>: Hindi, English</p>
+                    </div>
                 </div>
-                <div className="name-details">
-                    <p>Age    :</p>
-                    <p>25</p>
-                </div>
-                <div className="name-details">
-                    <p>Narionality    :</p>
-                    <p>Indian</p>
-                </div>
-                <div className="name-details">
-                    <p>Languages    :</p>
-                    <p>Hindi, English</p>
-                </div>
+                <button className="btn">Download Resume</button>
             </div>
         </div>
     )
